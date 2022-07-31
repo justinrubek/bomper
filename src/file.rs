970286eb933d9a4fbf382aa9c0d5da116931253b
Justin Rubek <justintrubek@protonmail.com>
2022-07-31 11:19:28 -0500

    refactor!: made overwrite_file accept and consume a PathBuf

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,6 @@
 use memmap::{Mmap, MmapMut};
-use std::{fs, fs::File, io::prelude::*, path::{Path, PathBuf}, ops::DerefMut};
+use rayon::prelude::*;
+use std::{fs, fs::File, io::prelude::*, path::PathBuf, ops::DerefMut, collections::HashSet};
 
 use crate::error::{Error, Result};
 
@@ -9,9 +10,9 @@ pub struct FileReplacer {
 
 }
 
-pub fn overwrite_file(path: &Path, old_content: &str, new_content: &str) -> Result<FileReplacer> {
-    let source_file = File::open(path)?;
-    let source_meta = fs::metadata(path)?;
+pub fn overwrite_file(path: PathBuf, old_content: &str, new_content: &str) -> Result<FileReplacer> {
+    let source_file = File::open(&path)?;
+    let source_meta = fs::metadata(&path)?;
     let source_map  = unsafe { Mmap::map(&source_file)? };
 
     let data = String::from(new_content).into_bytes();
@@ -22,8 +23,8 @@ pub fn overwrite_file(path: &Path, old_content: &str, new_content: &str) -> Resu
     let replaced = replace(&regex, &source_map, data);
 
     let temp_file = tempfile::NamedTempFile::new_in(
-        path.parent()
-        .ok_or_else(|| Error::InvalidPath(path.to_path_buf()))?,
+        (&path).parent()
+        .ok_or_else(|| Error::InvalidPath((&path).to_path_buf()))?,
     )?;
     let file = temp_file.as_file();
     file.set_len(replaced.len() as u64)?;
@@ -39,7 +40,7 @@ pub fn overwrite_file(path: &Path, old_content: &str, new_content: &str) -> Resu
     drop(source_file);
 
     Ok(FileReplacer {
-        path: path.to_path_buf(),
+        path,
         temp_file,
     })
 }

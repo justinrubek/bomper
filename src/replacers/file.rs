d7446a25aea2910383cfe2401df2103279ea9f69
Justin Rubek <--help>
2022-08-06 02:05:35 -0500

    feat: persist impl for FileReplacer which automatically persists that file

--- src/replacers/file.rs
+++ src/replacers/file.rs
@@ -1,6 +1,19 @@
+use std::fs;
 use std::path::PathBuf;
 
+use crate::error::Result;
+
+/// A replacer that contains a temporary file and a path it may be persisted to
 pub struct FileReplacer {
     pub path: PathBuf,
     pub temp_file: tempfile::NamedTempFile,
+}
+
+impl FileReplacer {
+    pub fn persist(self) -> Result<()> {
+        let path = fs::canonicalize(&self.path)?;
+        self.temp_file.persist(path)?;
+
+        Ok(())
+    }
 }

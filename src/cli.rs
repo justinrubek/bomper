6bc27e9c7f572c861b84409ce102676605d57c38
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-05-04 18:04:03 -0500

    refactor(cli)!: organize commands using subcommands
    This will allow for additional functionality to exist in bomper. The
    previous behavior is now under the `raw-bump` subcommand. This name
    denotes that its behavior is to only perform file updates, while future
    functionality will perform multiple operations.

--- src/cli.rs
+++ src/cli.rs
@@ -2,13 +2,36 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[clap(author, about, version)]
+#[command(author, about, version)]
 pub struct Args {
-    #[clap(short, long)]
-    pub config_file: Option<PathBuf>,
-    #[clap(short, long)]
-    pub dry_run: bool,
+    #[clap(flatten)]
+    pub base_args: BaseArgs,
 
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(clap::Args, Debug)]
+pub(crate) struct BaseArgs {
+    #[arg(short, long)]
+    pub config_file: Option<PathBuf>,
+    #[arg(short, long)]
+    pub dry_run: bool,
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub(crate) enum Commands {
+    /// bump versions in files without doing anything else
+    RawBump(RawBump),
+    /// bump versions in files and commit the changes
+    Bump(Bump),
+}
+
+#[derive(clap::Args, Debug)]
+pub(crate) struct RawBump {
     pub old_version: String,
     pub new_version: String,
 }
+
+#[derive(clap::Args, Debug)]
+pub(crate) struct Bump {}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ mod app;
 use app::App;
 
 mod cli;
-use cli::Args;
+use cli::{Args, Commands};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let app: App = App::new(args)?;
-    app.run()?;
-
+    let app = App::new(args.base_args)?;
+    match args.command {
+        Commands::RawBump(raw_bump) => {
+            app.raw_bump(&raw_bump)?;
+        }
+        Commands::Bump(bump) => {
+            app.bump(&bump)?;
+        }
+    }
     Ok(())
 }

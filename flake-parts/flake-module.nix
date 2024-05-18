ed31a0f6857f8d3a094868cd82883670a14dd680
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-05-17 22:47:50 -0500

    chore(flake-module): clean nix code

--- flake-parts/flake-module.nix
+++ flake-parts/flake-module.nix
@@ -1,55 +1,50 @@
 localFlake: {
-  self,
   lib,
   flake-parts-lib,
-  inputs,
   ...
 }: {
-  options = {
-    perSystem = flake-parts-lib.mkPerSystemOption ({
-      config,
-      inputs',
-      pkgs,
-      system,
-      ...
-    }: let
-      cfg = config.bomper;
-    in {
-      options = {
-        bomper = {
-          enable = lib.mkOption {
-            type = lib.types.bool;
-            default = false;
-            description = ''
-              Whether to enable generation bomper.
-            '';
-          };
+  options.perSystem = flake-parts-lib.mkPerSystemOption ({
+    config,
+    pkgs,
+    system,
+    ...
+  }: let
+    cfg = config.bomper;
+  in {
+    options = {
+      bomper = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = ''
+            Whether to enable generation bomper.
+          '';
+        };
 
-          configuration = lib.mkOption {
-            type = lib.types.lines;
-            description = ''
-              The contents of the configuration file to pass to bomper.
-            '';
-          };
+        configuration = lib.mkOption {
+          type = lib.types.lines;
+          description = ''
+            The contents of the configuration file to pass to bomper.
+          '';
+        };
 
-          wrappedBomper = lib.mkOption {
-            type = lib.types.package;
-            readOnly = true;
-            description = "A wrapped version of bomper with configuration already specified.";
-          };
+        wrappedBomper = lib.mkOption {
+          description = "A wrapped version of bomper with configuration already specified.";
+          readOnly = true;
+          type = lib.types.package;
         };
       };
+    };
 
-      config = let
-        configFile = pkgs.writeText "bomper-config" cfg.configuration;
-      in
-        lib.mkIf cfg.enable {
-          bomper.wrappedBomper = localFlake.withSystem system ({config, ...}: (pkgs.writeShellApplication {
-            name = "bomp";
-            runtimeInputs = [config.packages.cli];
-            text = ''exec bomp --config-file ${configFile} "$@"'';
-          }));
-        };
-    });
-  };
+    config = let
+      configFile = pkgs.writeText "bomper-config" cfg.configuration;
+    in
+      lib.mkIf cfg.enable {
+        bomper.wrappedBomper = localFlake.withSystem system ({config, ...}: (pkgs.writeShellApplication {
+          name = "bomp";
+          runtimeInputs = [config.packages.cli];
+          text = ''exec bomp --config-file ${configFile} "$@"'';
+        }));
+      };
+  });
 }
